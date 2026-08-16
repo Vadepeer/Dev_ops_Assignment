67ba5474909269e126152548477232913365ce1d
Vadepeer04 <102405641+Vadepeer@users.noreply.github.com>
2026-08-16 23:01:00 +0530

    Add Python file writing assignment

---
+++ Python_and_Bash_VadePeer/write_file.py
@@ -0,0 +1,7 @@
+file = open("student_notes.txt", "w")
+file.write("Python file handling practice.\n")
+file.write("This file was created using open() and write().\n")
+file.write("Writing to files is an important Python skill.\n")
+file.close()
+
+print("Content written successfully to student_notes.txt")
